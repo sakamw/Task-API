94f205e695c09b6c0e5b5a03353c50e926093e6c
sakamw <shanmwangi2020@gmail.com>
2025-06-12 20:29:57 +0300

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 
 const app = express();
